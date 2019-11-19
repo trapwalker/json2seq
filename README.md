79099f6fc0ec3cf3af4ca084b90b5133752e4d07
svp <svpmailbox@gmail.com>
2019-11-19 11:42:53 +0300

    REFACTORING

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # json2seq
-CLI tool to convert large JSON files into the JSON Text Sequence format (RFC 7464)
+CLI tool to convert large JSON files into the JSON Text Sequence format [RFC 7464](https://tools.ietf.org/html/rfc7464)
--- json2seq.py
+++ json2seq.py
@@ -5,9 +5,10 @@ import json
 import sys
 import argparse
 from itertools import islice
+from contextlib import suppress
 
 
-parser = argparse.ArgumentParser(description='Convert JSON to JSON Text Sequence.')
+parser = argparse.ArgumentParser(description='Convert JSON to JSON Text Sequence (RFC 7464).')
 parser.add_argument(
     'infile',
     type=argparse.FileType('r'), nargs='?', default=sys.stdin, help='Input JSON file (STDIN by default).')
@@ -50,45 +51,36 @@ class UpdatingError(ConvertingError):
 
 
 def safe_stringify_json(js):
-    try:
+    with suppress(Exception):
         return json.dumps(js, indent=2)
-    except:
-        return repr(js)
+    return repr(js)
 
 
-def filter_func(args):
-    fltr = args.fltr
-    filter_error = args.filter_error.lower()
-
+def filter_func(expr, on_error='strict'):
     def func(r):
         try:
-            res = eval(fltr, {}, r)
+            return eval(expr, {}, r)
         except Exception as e:
-            if filter_error == 'skip':
+            if on_error == 'skip':
                 return False
-            if filter_error == 'keep':
+            if on_error == 'keep':
                 return True
-            raise FilteringError(f'Filtering ERROR in expression {fltr!r}: {e}\nOn item:\n{safe_stringify_json(r)}')
-        else:
-            return res
+            raise FilteringError(f'Filtering ERROR in expression {expr!r}: {e}\nOn item:\n{safe_stringify_json(r)}')
 
     return func
 
 
-def update_func(args):
-    upd = args.upd
-    update_error = args.update_error.lower()
-
+def update_func(*codes, on_error: str = 'strict'):
     def func(r):
-        for f in upd:
+        for f in codes:
             try:
                 exec(f, {}, r)
             except Skip:
                 return None
             except Exception as e:
-                if update_error == 'skip':
+                if on_error == 'skip':
                     return None
-                if update_error == 'keep':
+                if on_error == 'keep':
                     return r
                 raise UpdatingError(f'Updating ERROR in expression {f!r}: {e}\nOn item:\n{safe_stringify_json(r)}')
             else:
@@ -99,13 +91,12 @@ def update_func(args):
 
 def main(argv: list = None):
     args = parser.parse_args(argv)
-    # print(args, file=sys.stderr)
     items = ijson.items(args.infile, args.selector)
     if args.fltr:
-        items = filter(filter_func(args), items)
+        items = filter(filter_func(args.fltr, on_error=args.filter_error.lower()), items)
 
     if args.upd:
-        items = filter(lambda r: r is not None, map(update_func(args), items))
+        items = (r for r in map(update_func(*args.upd, on_error=args.update_error.lower()), items) if r is not None)
 
     items = islice(items, args.skip, args.first and args.first + args.skip)
 
